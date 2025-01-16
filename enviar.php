5a29773dc467ef8bc9da556a7bcf898ab637ea10
Emanuel Herrera <71576817+Kurobaza96@users.noreply.github.com>
2025-01-15 22:36:58 -0600

    email1

--- enviar.php
+++ enviar.php
@@ -1,42 +1,12 @@
 <?php
+    ini_set( 'display_errors', 1 );
+    error_reporting( E_ALL );
 
-
-$nombre = $_POST['form-name']; 
-$correo = $_POST['form-email'];
-$mensaje = $_POST['form-message'];
-
-
-$destinatario = "emanuel.josue.herrera@gmail.com"; 
-$asunto = $_POST['form-subject'];
-$cuerpo = '
-    <html>
-        <head>
-            <title>Prueba de envio de correo</title>
-        </head>
-
-        <body>
-            <h1>Mi Pagina </h1>
-            <p>
-                Contacto: '.$nombre . + - + . $asunto . ' <br>
-
-                Mensaje: '.$mensaje.'
-
-            </p>
-
-        </body>
-
-    </html>
-';
-
-//para el envío en formato HTML
-$headers = "MIME-Version: 1.0\r\n";
-$headers."Content-type: text/html; charset=UTF8\r\n";
-
-//dirección del remitente
-$headers. "FROM: $nombre <$correo>\r\n"; mail($destinatario, $asunto, $cuerpo, $headers);
-
-echo "Correo enviado";
-
+    $from = $_POST['form-email'];
+    $to = "emanuel.josue.herrera@gmail.com";
+    $subject = $_POST['form-message'];
+    $message = "PHP mail works just fine";
+    $headers = "From:" . $from;
+    mail($to,$subject,$message, $headers);
+    echo "The email message was sent.";
 ?>
-
-<a href="index.html">Volver a inicio </a>
